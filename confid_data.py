05e179637960f943a0d0a72020b91af8cf44e4b9
StMuravko <73041096+StMuravko@users.noreply.github.com>
2022-05-09 14:01:54 +0300

    Delete confid_data.py

--- confid_data.py
+++
@@ -1,4 +0,0 @@
-BOT_TOKEN = "5367853662:AAFpR2vp7BWsBVwIiyrPHzOIcjmdkeOv8eo"
-ADMINS = 434977493, 327992639
-
-BD_URL = "postgres://vizfqpbpovjnpe:cfabb9a1d325f2a9fb44d5800c6dd622e8afc6815d045a9ec0ac308504d316cf@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d10o532hfl64ml"
