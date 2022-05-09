80b13bbf23845642fe75aa8208a99ecc35c00d7a
StMuravko <s.muravko@gmail.com>
2022-05-09 13:32:18 +0300

    deploy

--- confid_data.py
+++ confid_data.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "5367853662:AAFpR2vp7BWsBVwIiyrPHzOIcjmdkeOv8eo"
 ADMINS = 434977493, 327992639
 
-BD_URL = "postgres://kzhbvbgpxyptnd:9b5c265c88354245d338c2b6b636ee5b09a9be8d1ac018c9be4b2d238f4c08fa@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/das69bgbch2php"
+BD_URL = "postgres://vizfqpbpovjnpe:cfabb9a1d325f2a9fb44d5800c6dd622e8afc6815d045a9ec0ac308504d316cf@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d10o532hfl64ml"
